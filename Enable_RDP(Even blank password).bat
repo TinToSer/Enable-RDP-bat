14db8d045666276a4f2d8ebfc7bf3a8bf6323cb7
TinToSer <52107530+TinToSer@users.noreply.github.com>
2024-01-11 14:42:01 +0100

    Update Enable_RDP(Even blank password).bat

--- Enable_RDP(Even blank password).bat
+++ Enable_RDP(Even blank password).bat
@@ -4,8 +4,8 @@ REM Consolidator:https://github.com/tintoser
 REM # for enabling Remote Desktop
 reg add "HKLM\System\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0x00000000 /f
 
-REM # for disabling Network Level Authentication to have RDP feasible in local scenario also
-reg add "HKLM\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-TCP" /v UserAuthentication /t REG_DWORD /d 0x00000000 /f
+REM # for enabling Network Level Authentication, you can set last bit to 0 to disable it
+reg add "HKLM\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-TCP" /v UserAuthentication /t REG_DWORD /d 0x00000001 /f
 
 REM # for opening firewall port for RDP
 netsh advfirewall firewall set rule group="remote desktop" new enable=Yes
